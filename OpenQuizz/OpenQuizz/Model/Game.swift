273a622e2ac028086a78999baec6f2da3da1411f
MelObr <46160534+MelanieObr@users.noreply.github.com>
2019-06-11 15:03:37 +0200

    Add note pdf and files

---
+++ OpenQuizz/OpenQuizz/Model/Game.swift
@@ -0,0 +1,58 @@
+//
+//  Game.swift
+//  OpenQuizz
+//
+//  Created by Mélanie Obringer on 13/04/2019.
+//  Copyright © 2019 Mélanie Obringer. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    var score = 0
+    var state: State = .ongoing
+    
+    enum State {
+        case ongoing, over
+    }
+    
+    private var questions = [Question]()
+    private var currentIndex = 0
+    
+    var currentQuestion: Question {
+        return questions[currentIndex]
+    }
+    
+    func refresh() {
+        score = 0
+        currentIndex = 0
+        state = .over
+        
+        QuestionManager.shared.get { (questions) in
+            self.questions = questions
+            self.state = .ongoing
+            let name = Notification.Name(rawValue: "QuestionsLoaded")
+            let notification = Notification(name: name)
+            NotificationCenter.default.post(notification)
+        }
+    }
+    
+    func answerCurrentQuestion(with answer: Bool) {
+        if (currentQuestion.isCorrect && answer) || (!currentQuestion.isCorrect && !answer) {
+            score += 1
+        }
+        goToNextQuestion()
+    }
+    
+    private func goToNextQuestion() {
+        if currentIndex < questions.count - 1 {
+            currentIndex += 1
+        } else {
+            finishGame()
+        }
+    }
+    
+    private func finishGame() {
+        state = .over
+    }
+}
---
+++ OpenQuizz/OpenQuizz/Model/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  OpenQuizz
+//
+//  Created by Mélanie Obringer on 12/04/2019.
+//  Copyright © 2019 Mélanie Obringer. All rights reserved.
+//
+
+import Foundation
+struct Question {
+    var title = ""
+    var isCorrect = false
+}
